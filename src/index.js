9ec29fd928ebe20d66f064b99513118a01ec0c62
Orion55 <grol55@mail.ru>
2020-04-16 13:00:34 +0500

    Чтение конфига из файла #02

--- src/index.js
+++ src/index.js
@@ -1,11 +1,15 @@
 import readConfig from './libs/readConfig';
 
+const path = require('path');
 const clc = require('cli-color');
+
+const appRootPath = process.cwd();
 
 console.log(clc.blue('Начало работы скрипта'));
 
 const main = async () => {
-  const config = await readConfig('config.json');
+  const fullNameConfig = path.join(appRootPath, 'config.json');
+  const config = await readConfig(fullNameConfig);
   console.log(config);
 };
 
