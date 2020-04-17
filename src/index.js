6bb060dcc7ec9f02f58ab20d310b52d8f248d3ac
Orion55 <grol55@mail.ru>
2020-04-17 12:30:08 +0500

    Создание логгера

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 import readConfig from './libs/readConfig';
 import createDirs from './libs/createDirs';
+import logger from './libs/winston';
 
 const path = require('path');
 const clc = require('cli-color');
@@ -7,6 +8,8 @@ const clc = require('cli-color');
 const appRootPath = process.cwd();
 
 console.log(clc.blue('Начало работы скрипта'));
+logger.info('Hello world');
+logger.debug('Debugging info');
 
 const main = async () => {
   const fullNameConfig = path.join(appRootPath, 'config.json');
